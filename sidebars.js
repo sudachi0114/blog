a5e4229e6c549d6d582c5eab60575004e3a01a03
sudachi0114 <jnevariste@gmail.com>
2021-02-18 18:54:32 +0900

    サイドバーに階層構造を持たせる (help: https://v2.docusaurus.io/docs/sidebar#creating-a-hierachy)

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,11 @@ module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
-    git: ['git/multiUser/git-multiUser']
+    git: ['git/multiUser/git-multiUser'],
+    infra: [
+      {
+        apache:['infra/apacheServer/infra-apacheServer'],
+      },
+    ],
   },
 };
